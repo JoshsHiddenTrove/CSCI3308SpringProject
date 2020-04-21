446e1b1ca6b02ba003258223a78391a4d8deec6e
bwa876 <60196095+bwa876@users.noreply.github.com>
2020-04-20 21:11:45 -0500

    Add files via upload

---
+++ create_db_draft3.sql
@@ -0,0 +1,49 @@
+-- create recipe table
+CREATE TABLE recipe(
+	recipe_id SERIAL NOT NULL PRIMARY KEY, -- NB: serial datatype is implicitly UNIQUE --
+	name TEXT NOT NULL,
+	picture TEXT,
+	saved BOOLEAN NOT NULL -- NB: 'saved' corresponds to a user 'liking' a given recipe
+
+);
+\copy recipe(name, picture, saved) FROM '~/ShareSync/CS/3308/project/recipesTableData.csv' DELIMITER ',' CSV; -- psql function to import/copy data from external csv file into the database
+
+
+--create details table
+CREATE TABLE details(
+
+	-- NB: as of this, draft 3, primary key has been left out. Only using foreign key references, as the primary key will always been the same as the foreign key.
+	-- BUT, this is subject to change
+	cook_time TEXT,
+	servings INTEGER,
+	cals INTEGER,
+	directions TEXT NOT NULL,
+	recipe_id SERIAL REFERENCES recipe(recipe_id) ON DELETE CASCADE
+
+);
+\copy details(cook_time, servings, cals, directions) FROM '~/ShareSync/CS/3308/project/detailsTableData.csv' DELIMITER ';' CSV; -- Not sure if this was entirely necessary, but swapped delimiter to semicolon to ensure there were no insertion errors (ingredients has commas)
+
+
+--create stats table
+CREATE TABLE stats (
+
+	-- Again ignoring PK for now
+	num_likes INTEGER NOT NULL,
+	num_dislikes INTEGER NOT NULL,
+	recipe_id SERIAL REFERENCES recipe(recipe_id) ON DELETE CASCADE
+);
+\copy stats(num_likes,num_dislikes) FROM '~/ShareSync/CS/3308/project/statsTableData.csv' DELIMITER ',' CSV;
+
+
+-- create preferences table
+CREATE TABLE preferences (
+
+	vegan BOOLEAN NOT NULL,
+	dairy_free BOOLEAN NOT NULL,
+	vegetarian BOOLEAN NOT NULL,
+	gluten_free BOOLEAN NOT NULL,
+	nut_free BOOLEAN NOT NULL,
+	recipe_id SERIAL REFERENCES recipe(recipe_id) ON DELETE CASCADE
+);
+\copy preferences(vegan, dairy_free, vegetarian, gluten_free, nut_free) FROM '~/ShareSync/CS/3308/project/preferencesTableData.csv' DELIMITER ',' CSV;
+-- alter tables: add foregin key constraints
---
+++ parseScrape3.cpp
@@ -0,0 +1,205 @@
+/**
+For this parser to work correctly: Ingredients list must be captured within double quotes
+*/
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string>
+#include<vector>
+using namespace std;
+void parse(string filename)
+{
+	ifstream inFile; //ceate file stream
+	inFile.open(filename);
+	if(inFile.fail())
+	{
+		cout << "ERROR: Failed to open file passed" << endl;
+	}
+	else
+	{
+		string line;
+		string tempName;
+		string tempPicturePath;
+		string tempCookTime;
+		string tempServings;
+		string tempCals;
+		string tempIngredients;
+		vector <string> recipeNames;
+		vector <string> picturePaths;
+		vector <string> cooktimes;
+		vector <int> servings;
+		vector <int> cals;
+		vector <string> ingredients;
+		bool parsed = false;
+
+		while(getline(inFile, line))
+		{
+			
+			if(line != "") //if non empty line... This should skip and blank errors in the scrape process
+			{
+				stringstream ss_line(line); //create stringstream for entire line
+				getline(ss_line, tempName,',');
+				getline(ss_line, tempPicturePath,',');
+				getline(ss_line,tempCookTime,',');
+				getline(ss_line,tempServings,',');
+				getline(ss_line,tempCals,',');
+				getline(ss_line, tempIngredients);
+
+				recipeNames.push_back(tempName);
+				picturePaths.push_back(tempPicturePath);
+				cooktimes.push_back(tempCookTime);
+				int tempIntServings = stoi(tempServings);
+				int tempIntCals = stoi(tempCals);
+
+				servings.push_back(tempIntServings);
+				cals.push_back(tempIntCals);
+				ingredients.push_back(tempIngredients);
+				
+			}
+			parsed = true;
+		}
+
+
+		if(parsed)
+		{
+			ofstream writeToRecipes;
+			writeToRecipes.open("recipesTableData.csv");
+			if(!writeToRecipes.fail())
+			{
+				int i = 0;
+				while(i < recipeNames.size())
+				{
+					writeToRecipes << recipeNames.at(i) << "," << tempPicturePath << "," << "0" << endl;
+					i++;
+				}
+				writeToRecipes.close();
+			}
+			else
+			{
+				cout << "ERROR: Failed to write recipes data" << endl;
+			}
+
+			ofstream writeToDetails;
+			writeToDetails.open("detailsTableData.csv");
+			if(!writeToDetails.fail())
+			{
+				int j = 0;
+				while( (j < cooktimes.size() ) || (j < servings.size() ) || (j < cals.size() ) || ( j < ingredients.size() ) ) 
+				{
+				    writeToDetails << cooktimes.at(j) << ";" << servings.at(j) << ";" << cals.at(j) << ";"  << ingredients.at(j) << endl;
+				    j++;
+				}
+				writeToDetails.close();
+
+			}
+			else
+			{
+				cout << "ERROR: Failed to write details data" << endl;
+			}
+
+
+
+			ofstream writeToStats;
+			writeToStats.open("statsTableData.csv");
+			if(!writeToStats.fail())
+			{
+				int i = 0;
+				while(i < recipeNames.size())
+				{
+					writeToStats << "0,0" << endl;
+					i++;
+				}
+				writeToStats.close();
+			}
+			else
+			{
+				cout << "ERROR: Failed to write stats data" << endl;
+			}
+
+			
+
+			ofstream writeToPreferences;
+			writeToPreferences.open("preferencesTableData.csv");
+			if(!writeToPreferences.fail())
+			{
+
+				int i = 0;
+				while(i < recipeNames.size())
+				{
+					string isVegan = "";
+					string isGluten_free= "";
+					string isNut_free= "";
+					string isVegetarian= "";
+					string isDairy_free= "";
+
+					string currName = recipeNames.at(i);
+
+					if (currName.find("Vegan") <= currName.length() ) //vegan exists in title 
+					{
+						isVegan = "1";
+					}
+					else 
+					{
+						isVegan = "0";
+					}
+
+					if ((currName.find("Gluten Free") <= currName.length() ) || (currName.find("gluten free") <= currName.length() )) //vegan exists in title 
+					{
+						isGluten_free = "1";
+					}
+					else 
+					{
+						isGluten_free = "0";
+					}
+
+					if ((currName.find("Nut Free") <= currName.length() ) || (currName.find("nut free") <= currName.length() )) //vegan exists in title 
+					{
+						isNut_free = "1";
+					}
+					else 
+					{
+						isNut_free = "0";
+					}
+
+					if (currName.find("Vegetarian") <= currName.length() ) //vegan exists in title 
+					{
+						isVegetarian = "1";
+					}
+					else 
+					{
+						isVegetarian = "0";
+					}
+
+					if ((currName.find("Dairy Free") <= currName.length() ) || (currName.find("dairy free") <= currName.length() )) //vegan exists in title 
+					{
+						isDairy_free = "1";
+					}
+					else 
+					{
+						isDairy_free = "0";
+					}
+
+					writeToPreferences << isVegan << "," << isDairy_free << "," << isVegetarian << "," << isGluten_free << "," << isNut_free << endl;
+
+					
+					i++;
+				}
+				writeToPreferences.close();
+
+			}
+			else
+			{
+				cout << "ERROR: Failed to write preferences data" << endl;
+			}
+			
+		}
+		
+	}
+}
+
+
+int main()
+{
+	parse("parseText3.txt");
+	return 0;
+}
